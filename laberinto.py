60e92dbba6f8a6189e6cb3447db5a709a7c677c2
JPGenaro <jpgenaro06@gmail.com>
2023-10-05 08:46:23 -0300

    pequeños ajustes

--- laberinto.py
+++ laberinto.py
@@ -10,10 +10,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Juego de Laberinto - Rintobel")
 
 # Colores
-WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 # Función para cargar niveles desde un archivo
 def cargar_niveles(filename):
@@ -25,7 +26,7 @@ def cargar_niveles(filename):
 niveles = cargar_niveles("niveles.txt")
 
 # Tamaño de celda
-CELL_SIZE = WIDTH // len(niveles[0][0])
+CELL_SIZE = min(WIDTH // len(niveles[0][0]), HEIGHT // len(niveles[0]))
 
 # Nivel actual
 nivel_actual = 0
@@ -39,10 +40,15 @@ player_x, player_y = 1, 1
 # Mensaje de nivel actual
 font = pygame.font.Font(None, 36)
 
-# Mensaje de inicio
-inicio_font = pygame.font.Font(None, 72)
-inicio_texto = inicio_font.render("Rintobel", True, GREEN)
-inicio_rect = inicio_texto.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+# Mensaje de inicio del juego
+inicio_font = pygame.font.Font(None, 48)
+inicio_texto = inicio_font.render("Presiona la Barra Espaciadora para Empezar", True, WHITE)
+inicio_rect = inicio_texto.get_rect(center=(WIDTH // 2, HEIGHT * 0.8))
+
+# Mensaje del nombre del juego
+nombre_juego_font = pygame.font.Font(None, 72)
+nombre_juego_texto = nombre_juego_font.render("Rintobel", True, GREEN)
+nombre_juego_rect = nombre_juego_texto.get_rect(center=(WIDTH // 2, HEIGHT * 0.4))
 
 # Bucle principal del juego
 running = True
@@ -80,20 +86,25 @@ while running:
                 except IndexError:
                     pass  # Ignora intentos de moverse fuera de los límites del laberinto
 
+    # Dibujar el fondo negro
+    screen.fill(BLACK)
+
     # Dibujar el laberinto y al jugador
-    screen.fill(WHITE)
     if estado_juego == 1:
         for y, row in enumerate(niveles[nivel_actual]):
             for x, cell in enumerate(row):
                 if cell == "#":
+                    pygame.draw.rect(screen, YELLOW, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                elif cell == "E":
                     pygame.draw.rect(screen, RED, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
         pygame.draw.rect(screen, GREEN, (player_x * CELL_SIZE, player_y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     # Mostrar el número de nivel actual en pantalla
     if estado_juego == 1:
-        nivel_texto = font.render(f"Nivel {nivel_actual + 1}", True, BLACK)
+        nivel_texto = font.render(f"Nivel {nivel_actual + 1}", True, WHITE)
         screen.blit(nivel_texto, (10, 10))
     elif estado_juego == 0:  # Pantalla de inicio
+        screen.blit(nombre_juego_texto, nombre_juego_rect)
         screen.blit(inicio_texto, inicio_rect)
 
     pygame.display.update()
